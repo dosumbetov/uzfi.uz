81a867bda6546e32921f1e41b448c7744bfef708
Sardor Nugaev <sardor0418@mail.ru>
2021-11-28 13:06:32 +0500

    Owl Carousel

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -13,7 +13,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         
-        // 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+         'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
         '/venobox/venobox.css',
         'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
         '/newasset/css/plugins.css',
@@ -29,14 +29,14 @@ class AppAsset extends AssetBundle
         // 'http://code.jquery.com/jquery-latest.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js',
-        // 'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.1/jquery.min.js',
+         'https://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.1/jquery.min.js',
         // 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
         'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js',
         '/owl/dist/owl.carousel.min.js',
         '/newasset/js/plugins.js',
         '/newasset/js/theme.js',
         'https://unpkg.com/aos@2.3.1/dist/aos.js',
-        '/js/custom.js',
+//        '/js/custom.js',
         '/venobox/venobox.min.js',
         '/js/vanilla-tilt.min.js',
       
