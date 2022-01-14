c32a8168d745385d19709d69f3a65e82f4d02892
Dosumbetov Ogabek <dosumbetov19983010@gmail.com>
2022-01-14 10:03:02 +0500

    s

--- frontend/web/js/custom.js
+++ frontend/web/js/custom.js
@@ -77,53 +77,53 @@ setInterval(function(){
   }
 
 },2000);
-let snowmax = 30,
-    snowcolor = new Array("rgba(220,220,220,1)", "rgba(220,220,220,1)", "rgba(220,220,220,1)", "rgba(220,220,220,1)", "rgba(220,220,220,1)"),
-    snowletter = new Array("*","❆","❅","❄"),
-    sinkspeed = 0.6,
-    snowmaxsize = 30,
-    snowminsize = 8,
-    snow = new Array,
-    marginbottom,
-    marginright,
-    timer,
-    i_snow = 0,
-    x_mv = new Array,
-    crds = new Array,
-    lftrght = new Array;
+// let snowmax = 30,
+//     snowcolor = new Array("rgba(220,220,220,1)", "rgba(220,220,220,1)", "rgba(220,220,220,1)", "rgba(220,220,220,1)", "rgba(220,220,220,1)"),
+//     snowletter = new Array("*","❆","❅","❄"),
+//     sinkspeed = 0.6,
+//     snowmaxsize = 30,
+//     snowminsize = 8,
+//     snow = new Array,
+//     marginbottom,
+//     marginright,
+//     timer,
+//     i_snow = 0,
+//     x_mv = new Array,
+//     crds = new Array,
+//     lftrght = new Array;
 
-for (i = 0; i <= snowmax; i++)
-  document.write("<span id='s" + i
-    + "' class='snowflake' style='pointer-events:none;z-index:9999;position:fixed;top:-"
-    + snowmaxsize
-    + "'>"
-    + snowletter[randommaker(snowletter.length)]
-    + "</span>");
+// for (i = 0; i <= snowmax; i++)
+//   document.write("<span id='s" + i
+//     + "' class='snowflake' style='pointer-events:none;z-index:9999;position:fixed;top:-"
+//     + snowmaxsize
+//     + "'>"
+//     + snowletter[randommaker(snowletter.length)]
+//     + "</span>");
 
-window.onload = initsnow;
+// window.onload = initsnow;
 
-function randommaker(o) {
-  return rand = Math.floor(o * Math.random()), rand
-};
+// function randommaker(o) {
+//   return rand = Math.floor(o * Math.random()), rand
+// };
 
-function initsnow() {
-  marginbottom = window.innerHeight, marginright = window.innerWidth - 15;
-  var o = snowmaxsize - snowminsize;
-  for (i = 0; i <= snowmax; i++) crds[i] = 0, lftrght[i] = 15 * Math.random(), x_mv[i] = 0.03 + Math.random() / 10, snow[i] = document.getElementById("s" + i), snow[i].size = randommaker(o) + snowminsize, snow[i].style.fontSize = snow[i].size + "px", snow[i].style.color = snowcolor[randommaker(snowcolor.length)], snow[i].sink = sinkspeed * snow[i].size / 5, snow[i].posx = randommaker(marginright - snow[i].size), snow[i].posy = randommaker(2 * marginbottom - marginbottom - 2 * snow[i].size), snow[i].style.left = snow[i].posx + "px", snow[i].style.top = snow[i].posy + "px";
-  movesnow()
-};
+// function initsnow() {
+//   marginbottom = window.innerHeight, marginright = window.innerWidth - 15;
+//   var o = snowmaxsize - snowminsize;
+//   for (i = 0; i <= snowmax; i++) crds[i] = 0, lftrght[i] = 15 * Math.random(), x_mv[i] = 0.03 + Math.random() / 10, snow[i] = document.getElementById("s" + i), snow[i].size = randommaker(o) + snowminsize, snow[i].style.fontSize = snow[i].size + "px", snow[i].style.color = snowcolor[randommaker(snowcolor.length)], snow[i].sink = sinkspeed * snow[i].size / 5, snow[i].posx = randommaker(marginright - snow[i].size), snow[i].posy = randommaker(2 * marginbottom - marginbottom - 2 * snow[i].size), snow[i].style.left = snow[i].posx + "px", snow[i].style.top = snow[i].posy + "px";
+//   movesnow()
+// };
 
-function movesnow() {
-  for (i = 0; i <= snowmax; i++) crds[i]
-    += x_mv[i], snow[i].posy
-    += snow[i].sink, snow[i].style.left = snow[i].posx
-    + lftrght[i] * Math.sin(crds[i])
-    + "px", snow[i].style.top = snow[i].posy
-    + "px", (snow[i].posy >= marginbottom - 2 * snow[i].size || parseInt(snow[i].style.left) > marginright - 3 * lftrght[i]) && (snow[i].posx = randommaker(marginright - snow[i].size), snow[i].posy = 0);
+// function movesnow() {
+//   for (i = 0; i <= snowmax; i++) crds[i]
+//     += x_mv[i], snow[i].posy
+//     += snow[i].sink, snow[i].style.left = snow[i].posx
+//     + lftrght[i] * Math.sin(crds[i])
+//     + "px", snow[i].style.top = snow[i].posy
+//     + "px", (snow[i].posy >= marginbottom - 2 * snow[i].size || parseInt(snow[i].style.left) > marginright - 3 * lftrght[i]) && (snow[i].posx = randommaker(marginright - snow[i].size), snow[i].posy = 0);
 
-  setTimeout("movesnow()", 30)
-};
+//   setTimeout("movesnow()", 30)
+// };
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+// $('#myModal').on('shown.bs.modal', function () {
+//   $('#myInput').trigger('focus')
+// })
