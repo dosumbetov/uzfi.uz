0656407e92d636e98a2e753af8860afd1dcf17bd
Dosumbetov Ogabek <dosumbetov19983010@gmail.com>
2022-02-22 16:47:04 +0500

    asd

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -255,9 +255,7 @@ AppAsset::register($this);
           </div>
         </div>
       </div>
-      <?= Breadcrumbs::widget([
-          'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-      ]) ?>
+<!--  -->
     
     <!-- /section -->
   <!-- ##### Hero Area Start ##### -->
