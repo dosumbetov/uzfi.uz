f99897ba03f97848652287e8ffab150f083d7fac
Dosumbetov Ogabek <dosumbetov19983010@gmail.com>
2022-04-11 09:42:23 +0500

    sad

--- frontend/views/site/gallery.php
+++ frontend/views/site/gallery.php
@@ -64,128 +64,96 @@ $this->title = Yii::t('app', 'Aloqa')?>;
   }
   }
  </style>
-    <div class="-fx-image-gal">
 
-  <div class="-fx-gal-item">
-    <div class="-fx-gal-image-thumb" tabindex="1">
-      <img src="https://picsum.photos/id/237/300/300" />
-    </div>
-    <div class="-fx-gal-image-big">
-      <img src="https://picsum.photos/id/237/800/600" />
-    </div>
-  </div><!-- /-fx-gal-item -->
-
-  <div class="-fx-gal-item">
-    <div class="-fx-gal-image-thumb" tabindex="1">
-      <img src="https://picsum.photos/id/238/300/300" />
-    </div>
-    <div class="-fx-gal-image-big">
-      <img src="https://picsum.photos/id/238/800/600" />
-    </div>
-  </div><!-- /-fx-gal-item -->
-
-  <div class="-fx-gal-item">
-    <div class="-fx-gal-image-thumb" tabindex="1">
-      <img src="https://picsum.photos/id/39/300/300" />
-    </div>
-    <div class="-fx-gal-image-big">
-      <img src="https://picsum.photos/id/39/1024/768" />
-    </div>
-  </div><!-- /-fx-gal-item -->
-
-  <div class="-fx-gal-item">
-    <div class="-fx-gal-image-thumb" tabindex="1">
-      <img src="https://picsum.photos/id/240/300/300" />
-    </div>
-    <div class="-fx-gal-image-big">
-      <img src="https://picsum.photos/id/240/1920/1024" />
-    </div>
-  </div><!-- /-fx-gal-item -->
-
-  <div class="-fx-gal-item">
-    <div class="-fx-gal-image-thumb" tabindex="1">
-      <img src="https://picsum.photos/id/341/300/300" />
-    </div>
-    <div class="-fx-gal-image-big">
-      <img src="https://picsum.photos/id/341/800/600" />
-    </div>
-  </div><!-- /-fx-gal-item -->
-
-  <div class="-fx-gal-item">
-    <div class="-fx-gal-image-thumb" tabindex="1">
-      <img src="https://picsum.photos/id/142/300/300" />
-    </div>
-    <div class="-fx-gal-image-big">
-      <img src="https://picsum.photos/id/142/1920/1024" />
-    </div>
-  </div><!-- /-fx-gal-item -->
-
-</div><!-- /gallery -->
+<body>
+   <div class="seccion">
+    <div class="galeria">
+      <ul>
+        <li><img src="https://res.cloudinary.com/miweb10/image/upload/v1480451921/01-gal10_wfbbrk.jpg"></li>
+        <li><img src="https://res.cloudinary.com/miweb10/image/upload/v1480451921/01-gal9_dyhjrd.jpg"></li>
+          <li><img src="https://res.cloudinary.com/miweb10/image/upload/v1480451921/01-gal8_xyaglw.jpg"></li>
+        <li><img src="https://res.cloudinary.com/miweb10/image/upload/v1480451921/01-gal7_qezqc6.jpg"></li>
+        <li><img src="https://res.cloudinary.com/miweb10/image/upload/v1480451921/01-gal5_i77mw0.jpg"></li>
+        <li><img src="https://res.cloudinary.com/miweb10/image/upload/v1480451921/01-gal4_g8t3o8.jpg"></li>
+        <li><img src="https://res.cloudinary.com/miweb10/image/upload/v1480451921/01-gal3_eebtfm.jpg"></li>
+        <li><img src="https://res.cloudinary.com/miweb10/image/upload/v1480451921/01-gal2_jlxmnm.jpg"></li>
+        <li><img src="https://res.cloudinary.com/miweb10/image/upload/v1480451921/01-gal1_fbmz6u.jpg"></li>
+      </ul>
+   </div>
+  </div>
+</body>
+<footer>
+ <p id="copyright">Developed by Daniel Villalba<br><a href="https://d4ni.es">www.d4ni.es</a></p>
+</footer>
 <style type="text/css">
-    @import url("https://fonts.googleapis.com/css2?family=Mansalva&display=swap");
-
-
-/* gallery specific CSS */
-
-.-fx-image-gal {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  width: 85%; /* arbitrary valye */
-  margin: 0px auto;
+.seccion {
+  width: 60%;
+  margin-left: 20%;
 }
-.-fx-gal-item {
-  width: 10%; /* for 3 columns */
+
+.galeria {
+  margin-top: 40px;
+  margin-bottom: 40px;
+}
+
+.galeria ul li,  .galeria li img {
+  -webkit-transition: all 1s cubic-bezier(.42,.2,.2,.64);
+    -moz-transition: all 1s cubic-bezier(.42,.2,.2,.64);
+    -o-transition: all 1s cubic-bezier(.42,.2,.2,.64);
+    transition: all 1s cubic-bezier(..42,.2,.2,.64);
+}
+
+.galeria ul li {
+  position: relative;
+  float: left;
+  width: 140px;
+  height: 140px;
   margin: 5px;
-  overflow: hidden;
-  border-radius: 15px;
+  padding: 5px;
+  list-style-type: none;
 }
 
-.-fx-gal-image-thumb img {
-  width: 100%;
-  border-radius: 15px;
-  cursor: pointer;
-  -webkit-filter: grayscale(80%);
-  -moz-filter: grayscale(80%);
-  filter: grayscale(80%);
-  transition: all 0.3s ease;
+.galeria ul li:hover {
+  z-index: 99;
 }
 
-.-fx-gal-image-thumb:hover img {
-  width: 100%;
-  cursor: pointer;
+.galeria ul li img {
+  position: absolute;
+  left: 0;
+  top: 0;
+  border: 1px solid #999999;
+  padding: 5px;
+  width: 140px;
+  height: 140px;
+  background: #abc4d6;
+  filter: grayscale(100%);
+  -webkit-filter: grayscale(100%);
+  -moz-filter: grayscale(100%);
+  -ms-filter: grayscale(100%);
+  -o-filter: grayscale(100%);
+  border-radius: 75px;
+}
+
+.galeria ul li img:hover {
+  width: 280px;
+  height: 280px;
+  margin-top: -150px;
+  margin-left: -150px;
+  top: 50%;
+  left: 50%;
+  filter: grayscale(0%);
   -webkit-filter: grayscale(0%);
   -moz-filter: grayscale(0%);
-  filter: grayscale(0%);
-
-  transform: scale(1.2);
-  transition: all 0.5s ease;
+  -ms-filter: grayscale(0%);
+  -o-filter: grayscale(0%);
+  border-radius: 40px;
 }
 
-.-fx-gal-image-thumb:focus + .-fx-gal-image-big {
-  display: block;
+#copyright {
+  margin-top: 40px;
+  margin-right: 20%;
+  width: 60%;
+  text-align: right;
+  float: right;
 }
-
-.-fx-gal-image-big {
-  display: none;
-  position: fixed;
-  top: 0;
-  left: 0;
-  background-color: rgba(5, 10, 15, 0.8);
-  overflow: hidden;
-  height: 100vh;
-  width: 100vw;
-  z-index: 999;
-  transition: all 0.3s ease;
-}
-
-.-fx-gal-image-big img {
-  max-width: 90vw;
-  position: absolute;
-  box-shadow: 0px 0px 800px 40px rgba(0, 0, 0, 0.9);
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-}
-
 </style>
