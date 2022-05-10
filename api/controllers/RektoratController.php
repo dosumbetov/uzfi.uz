67889f625ede7b55b168c4065e275c1e5929c484
Sardor Nugaev <sardor0418@yandex.ru>
2022-05-10 10:15:19 +0500

    123

---
+++ api/controllers/RektoratController.php
@@ -0,0 +1,14 @@
+<?
+namespace api\controllers;
+
+use yii\rest\ActiveController;
+use yii\helpers\ArrayHelper;
+use backend\models\Rektorat;
+use yii\data\ActiveDataProvider;
+
+class RektoratController extends ActiveController
+{
+    public $modelClass = 'backend\models\Rektorat';
+}
+
+
