b24c67a98f0db45d958c29b4b4a91569bc3e1aa3
Dosumbetov Ogabek <dosumbetov19983010@gmail.com>
2022-09-12 08:48:03 +0500

    sa

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -19,7 +19,7 @@ $this->title = "SAMARQAND DAVLAT UNIVERSITETINING O'ZBEKISTON-FINLANDIYA PEDAGOG
           <div class="carousel-item active">
             <img src="/menu/images/imageslider.jpg" class="d-block w-100 rasm" alt="...">
             <div class="carousel-caption d-none d-md-block" style="text-align: right;">
-                <a target="_blank" href="https://www.youtube.com/watch?v=ADdQABZUcWY&ab_channel=UzFiEducation" class="btn btn-primary" style="font-size: 25px;">Conference Live</a>
+                <a target="_blank" href="https://360.openconference.us/uzfi/" class="btn btn-primary" style="font-size: 25px;">Conference Live</a>
             </div>
           </div>
        <?
@@ -28,7 +28,7 @@ $this->title = "SAMARQAND DAVLAT UNIVERSITETINING O'ZBEKISTON-FINLANDIYA PEDAGOG
              <div class="carousel-item">
                 <img src="arguments/slider/<?=$slide->slider_img?>" class="d-block w-100 rasm" alt="...">
                 <div class="carousel-caption d-none d-md-block" style="text-align: right;">
-                    <a target="_blank" href="https://www.youtube.com/watch?v=ADdQABZUcWY&ab_channel=UzFiEducation" class="btn btn-primary" style="font-size: 25px;">Conference Live</a>
+                    <a target="_blank" href="https://360.openconference.us/uzfi/" class="btn btn-primary" style="font-size: 25px;">Conference Live</a>
                 </div>
               </div>
           <?
